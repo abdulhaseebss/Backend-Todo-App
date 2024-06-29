1e2537c8fa4f9610b2da5e3c5df9099b0e2a9a4f
abdulhaseebss <m.saleemhaseeb@gmail.com>
2024-06-29 16:14:09 +0500

    more updates

--- index.mjs
+++ index.mjs
@@ -2,10 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { MONGO_URI, PORT } from './src/config/env.mjs';
 import routes from './src/routes/index.mjs';
-import cors from "cors"
+import cors from 'cors';
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, {
@@ -18,16 +18,22 @@ mongoose.connect(MONGO_URI, {
 mongoose.connection.once('open', () => {
     console.log("Connected to DB");
 
-    // Start the server after successful DB connection
+    // Middleware
+    app.use(express.json());
+
+    // Routes
+    app.use('/', routes);
+
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+
+    // Start the server after successful DB connection and setup
     app.listen(PORT, () => {
         console.log(`Server is running at port ${PORT}`);
     });
 }).on("error", (err) => {
-    console.log("Error connecting to DB -->", err);
+    console.error("Error connecting to DB -->", err);
 });
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/', routes);
